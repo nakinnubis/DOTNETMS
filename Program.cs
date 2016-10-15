bb0c4ff22620280c95199243796242839728c9e2
nakinnubis <abiola.akinnubi@gmail.com>
2016-10-15 05:55:42 +0100

    Still Part of my C# rivival
    I have been able to test features of c# 6 {variablename} to replace a
    string or value

--- Program.cs
+++ Program.cs
@@ -22,6 +22,32 @@ namespace ConsoleApplication
             
             //return 1;
            //return number;
+
+           //triming, concatination and string manipulation
+
+           string one = "one";
+           string two = "two";
+           string three = "123";
+           Console.WriteLine(one + two + three);
+
+           string Original = "The Great Programmer";
+           string Capital = Original.ToUpper();
+           string Lower = Original.ToLower();
+           Console.WriteLine(Original + " "+ Capital + " "+ Lower);
+           string tobetrim = " Programmers ";
+           string trimmed1  = tobetrim.TrimStart();
+           string trimmed2  = tobetrim.TrimEnd();
+           string trimmed3  = tobetrim.Trim();
+           Console.WriteLine("This is Original: "+tobetrim+ "\n This is trimmed1" +trimmed1+ "\n This is trimmed2"+trimmed2+"\n This is trimmed3"+trimmed3);
+
+           //using the substring shorthand version
+           string subsbtringVersion = tobetrim.Trim().Substring(0,10);
+           Console.WriteLine(subsbtringVersion);
+           string name = "Abiola";
+           string surname = "Akinnubi";
+           string greetme = String.Format("Hello {0} {1}!", name,surname); // Hello Abiola Akinnubi!
+           Console.WriteLine(greetme);
+
         }
     }
 }
