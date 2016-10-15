a6d9009e9eaae12bb2a9b8b2ee9430e5995b42de
nakinnubis <abiola.akinnubi@gmail.com>
2016-10-15 05:18:35 +0100

    Revising my C# Knowledge as my new Task will be there
    Here is just a respository of my quick brushup of c# and .net aiming for
    the best in my new job

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args) 
+         {
+            // int number = 40;
+             //Console.WriteLine(number);
+             var abiola = "Akinnubi Sylvester";  
+              var greetings =  "Welcome back to C# it's been a while";     
+              string  abbeycity = "Testing a string";      
+              if (abiola.Length != abbeycity.Length )
+              {
+                  Console.WriteLine("They are not of equal lengths");
+                  
+              }
+             //var greetings = "Welcome back to C# it's been a while";
+             Console.WriteLine($"{greetings} {abiola} ! & {abbeycity}");
+            
+            //return 1;
+           //return number;
+        }
+    }
+}
